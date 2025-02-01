80fcb2962bb2219d02b9a5346124f3c160c76148
Fardeen khan <113256054+fardeen-Git@users.noreply.github.com>
2025-02-01 18:33:30 +0000

    const,let,var

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,24 @@
+const accountId = 144556
+let accountEmail = "fardeen@gmail.com"
+var accountpassword = "123"
+accountcity = "jaipur";        
+
+/*
+   we mostly use const and let and should not choose to declare a variable without them. (like accountcity)
+*/
+
+// accountId = 2  ->not allowed to be assigned to a variable who is defined using const
+
+
+accountEmail = "khan@1gmail.com"
+accountpassword = "1234"
+accountcity = "delhi"
+
+console.log(accountId)
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountpassword,accountcity])
